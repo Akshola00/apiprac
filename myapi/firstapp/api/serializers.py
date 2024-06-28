9b7494bdc452947194382af20455b596189bf15f
Akshola00 <akinniyishola07@gmail.com>
2024-06-28 09:34:05 +0100

    modified:   myapi/firstapp/api/serializers.py

--- myapi/firstapp/api/serializers.py
+++ myapi/firstapp/api/serializers.py
@@ -3,5 +3,5 @@ from ..models import *
 
 class CarSpecsSerializer(serializers.ModelSerializer):
     class Meta:
-        model = CarSpecs
+        model = CarSpecs 
         fields = ['car_model', 'car_year', 'car_type', 'car_bodytype', 'car_engine']
